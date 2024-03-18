6b8c2aff54e6054c3ffce895a3ab6dd6fdcb9114
suresh <surehverma4443@gmail.com>
2024-03-18 17:40:06 +0530

    true and false

---
+++ 13.03.2024/tash.js
@@ -0,0 +1,29 @@
+Q.-1 what is the result of "true && false"
+ans:-false
+
+Q.-2 what is the result of "true|| false"
+ans:-true
+
+Q.3 what is the result of "5>3 && 10<20"
+ans:-true
+
+Q.4  what is the result of "10===10 || 5>8"
+ans:- false
+
+Q.-5  what is the result of "true && false" || "true"
+ans:- false
+
+Q.-6  what is the result of "false && (true || false)"
+ans- fasle
+
+Q.-7  what is the result of  (5<=5)&&(false||true)
+ans:- true
+
+Q.-8  what is the result of "true && true && false"
+ans:-false
+
+Q.-9  what is the result of "false|| false|| true"
+ans:- true
+
+Q.10  what is the result of 10>=5 && 3<2 || 8===8
+ans.- true
