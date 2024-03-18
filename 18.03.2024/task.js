da7ea3a4b8bed69dbbf76f28750e921d653926b3
suresh <surehverma4443@gmail.com>
2024-03-18 16:36:42 +0530

    question

---
+++ 18.03.2024/task.js
@@ -0,0 +1,38 @@
+// question-1
+let num1=10;
+let num2=15;
+if( num1>num2){
+    console.log("num1 is larger")
+}else if( num1<num2){
+    console.log("num1 is smaller")
+}else if( num1==num2){
+    console.log("num1 and num2 is equal")
+}else{
+    console.log("not valid")
+}
+
+
+// question-2
+
+let lightcolor="green"
+
+if(lightcolor="green"){
+    console.log("green")
+}else if(lightcolor="yellow"){
+    console.log("slow down")
+}else if(lightcolor="red"){
+    console.log("stop")
+}
+
+
+// question-3
+
+let age=25
+if(age<18){
+    console.log("Minor")  
+}else if(age>=18 && age<=65){
+    console.log("Adult")
+}else{
+    console.log(senior)
+}
+
