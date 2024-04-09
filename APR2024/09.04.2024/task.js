2a57a89e6b9efaf1ab458aab80ed20050798e75c
suresh <surehverma4443@gmail.com>
2024-04-09 14:39:10 +0530

    hj

---
+++ APR2024/09.04.2024/task.js
@@ -0,0 +1,151 @@
+// Q-1 how to print all Element of a String
+
+
+
+// let str="jaipur"
+// let bag=""
+// let i=0;
+
+// while(i<str.length){
+//     bag=bag+str[i]+" "
+//     i++
+   
+// }
+// console.log(bag)
+
+
+
+
+// Q-2 how to  horizental print all Element of an arry
+
+
+// let arr=[1,2,3,4,5,6,7,8]
+// let i=0;
+// let bag=""
+// while(i<arr.length){
+//     bag=bag+arr[i]+" "
+//     i++
+// }
+// console.log(bag)
+
+
+
+// Q-3 how to   print  sum of all Element of an arry
+
+
+// let arr=[1,2,3,4,5]
+// let i=0;
+// let sum=0;
+// while(i<arr.length){
+//     sum=sum+arr[i]
+//     i++
+// }
+// console.log(sum)
+
+
+
+// Q-4 how to   print  all even number of an arry
+
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+//  let i=0;
+// let bag ="";
+// while(i<arr.length){
+//     if(arr[i]%2==0){
+//         bag=bag+arr[i]+" "  
+//     }
+//     i++    
+// }
+// console.log(bag)
+
+
+// Q-5 how to   print  all odd number of an arry
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+// let i=0;
+// let bag ="";
+// while(i<arr.length){
+//      if(arr[i]%2==1){
+//         bag=bag+arr[i]+" "  
+//      }
+//     i++    
+//  }
+//  console.log(bag)
+
+
+
+
+//  Q-6 how to   print   count of all even number of an arry
+
+
+//  let arr=[1,2,3,4,5,6,7,8,9]
+//   let i=0;
+//  let count =0;
+//  while(i<arr.length){
+//      if(arr[i]%2==0){
+//          count++ 
+//      }
+//      i++    
+//  }
+//  console.log(count)
+
+
+//  Q-7  count how many times 2 print inside an arry
+
+// let arr=[1,2,3,2,5,2,7,8,2]
+//    let i=0;
+//   let twocount =0;
+//   while(i<arr.length){
+//       if(arr[i]==2){
+//           twocount++ 
+//      }
+//       i++    
+//  }
+//   console.log(twocount)
+
+
+
+
+//  Q-8 how to   print  sum all even number of an arry
+
+  
+//  let arr=[1,2,3,4,5,6,7,8,9]
+//  let i=0;
+//  let evensum =0;
+//  while(i<arr.length){
+//      if(arr[i]%2==0){
+//           evensum=evensum+arr[i]
+//      }
+//      i++    
+//  }
+//  console.log(evensum)
+
+
+//   Q-9 how to   print  count,sum,average all even number of an arry
+
+  
+  let arr=[1,2,3,4,5,6,7,8,9]
+  let i=0;
+  let count=0;
+  let sum =0;
+  let average =0;
+  while(i<arr.length){
+      if(arr[i]%2==0){
+        count++
+        sum=sum+arr[i]
+      }
+      i++    
+  }
+  average=sum/count
+  console.log(count,sum,average)
+
+
+
+
+
+
+
+
+
+
+
